52f313c1d5da56a5a95f784d35ee05be1b6f875b
JinganGuo <gja1026@163.com>
2018-07-13 18:32:37 +0800

    编辑swiper组件

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="img-container">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in imgList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" :alt="item.imgAlt" >
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSwiper',
+    data: function () {
+      return {
+        swiperOption: {
+            pagination: '.swiper-pagination',
+            loop: true,
+            autoplay: 3000
+        },
+        imgList: [
+            {
+                id: "001",
+                imgUrl: "http://img1.qunarzz.com/piao/fusion/1807/ec/278a34afb59a0d02.jpg_750x200_3292a705.jpg",
+                imgAlt: "远去的恐龙"
+            },
+            {
+                id: "002",
+                imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/f5/ea83238744f31102.jpg_750x200_73b8477c.jpg",
+                imgAlt: "卡塔尔的讲述"
+            }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.img-container >>> .swiper-pagination-bullet-active
+    background-color: #ffffff
+.img-container
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.66%    
+    background: #cccccc 
+    .swiper-img 
+        width: 100%
+</style>
