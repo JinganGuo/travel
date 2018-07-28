954c7a5648b0aaeb0f3e62b70a78bbc72ab53280
JinganGuo <gja1026@163.com>
2018-07-28 22:40:16 +0800

    edit banner component

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="banner">
         <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_600x330_5d562f69.jpg" alt="banner">
+        banner
     </div>
 </template>
 
