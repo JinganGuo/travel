6d21b4f8280eca565e94d9ebf96337724ee5fd8a
JinganGuo <gja1026@163.com>
2018-07-29 14:04:27 +0800

    todo: detail pages

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="wrapper">
+            <swiper>
+            <!-- slides -->
+            <swiper-slide>
+                <img class="swiper-img"  src="" >
+            </swiper-slide>
+            <!-- Optional controls -->
+            <!-- <div class="swiper-pagination" slot="pagination"></div>
+            </swiper> -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CommonGallary"
+    }    
+</script>
+
+<style lang="stylus" scoped>
+    .container
+        display flex
+        flex-direction column
+        justify-content center
+        z-index 999
+        position fixed
+        left 0 
+        right 0
+        top 0
+        bottom 0
+        background #000
+        .wrapper 
+            overflow hidden
+            width 100%
+            height 0
+            padding-bottom  100%
+            
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="banner">
         <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_600x330_5d562f69.jpg" alt="banner">
-        banner
+        <div class="banner-info">
+            <div class="banner-title">金海湖风景区</div>
+            <div class="banner-number">
+                <span class="iconfont banner-icon">&#xe624;</span>
+                5
+            </div>
+        </div>
+        <common-gallary></common-gallary>
     </div>
 </template>
 
 <script>
+    import CommonGallary from 'common/gallary/Gallary'
     export default {
-        name: "DetailBanner"
+        name: "DetailBanner",
+        components: {
+            CommonGallary
+        }
     }
 </script>
 
@@ -16,7 +27,33 @@
         overflow hidden
         height 0
         padding-bottom 55%
+        position relative
         .banner-img
             width 100% 
+        .banner-info
+            display flex
+            position absolute
+            left 0
+            right  0
+            bottom  0
+            line-height .6rem
+            color  #ffffff
+            background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+            .banner-title
+                flex 1
+                font-size .32rem
+                padding 0 .2rem
+            .banner-number 
+                padding 0 .4rem
+                line-height .4rem
+                height  .32rem
+                margin-top .14rem
+                border-radius .32rem
+                font-size .24rem
+                background rgba(0, 0, 0, .9)
+                .banner-icon 
+                    font-size .24rem
+            
+
 </style>
 
